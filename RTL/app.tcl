30867cd2c1ab9c4db59443a3603bbd55890b3353
Richard Toth <richard.toth@lombiq.com>
2017-11-01 18:22:57 +0100

    Hast_IP.vhd and Hast_ip_wrapper.vhd files added to the Quartus project.
    --HG--
    branch : dev

--- RTL/app.tcl
+++ RTL/app.tcl
@@ -2,3 +2,5 @@ set_global_assignment -name SYSTEMVERILOG_FILE $app_path/Role.sv
 set_global_assignment -name SYSTEMVERILOG_FILE $app_path/SimpleDram.sv
 set_global_assignment -name SYSTEMVERILOG_FILE $app_path/DramInterleaver.sv
 set_global_assignment -name SYSTEMVERILOG_FILE $app_path/SimpleRole.sv
+set_global_assignment -name VHDL_FILE $app_path/Hast_IP.vhd
+set_global_assignment -name VHDL_FILE $app_path/Hast_ip_wrapper.vhd
