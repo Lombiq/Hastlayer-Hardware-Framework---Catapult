0516968406a9f02ad80418851c7bc5840bd49605
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-10-01 16:38:56 +0200

    Making RunDesign more flexible: it now lets you run it not just from under the default Roles path
    --HG--
    branch : issue/HAST-134

--- RunDesign.ps1
+++ RunDesign.ps1
@@ -1,8 +1,13 @@
-cd E:\catapult\v1.2\Roles
-Remove-Item .\hastlayerhardware.rpd
-.\genRpd.ps1 -inputSof .\HastlayerHardwareFramework-Catapult\Project\output_files\Project.sof -outputRpd .\hastlayerhardware.rpd
-cd E:\catapult\v1.2\Driver\Bin
-.\RSU.exe -write E:\catapult\v1.2\Roles\hastlayerhardware.rpd
+if (Test-Path Hastlayer.rpd) {
+    Remove-Item .\Hastlayer.rpd
+}
+
+# genRpd doesn't seem to work when executed from the Hastlayer folder, so need to browse there.
+cd ..
+.\genRpd.ps1 -inputSof $PSScriptRoot\Project\output_files\Project.sof -outputRpd .\Hastlayer.rpd
+
+cd ..\Driver\Bin
+.\RSU.exe -write ..\..\Roles\Hastlayer.rpd
 .\RSU.exe -reconfig
-#cd E:\Catapult\v1.2\Software\LoopbackStressTest\x64\Release
-#.\LoopbackStressTest.exe
+
+cd $PSScriptRoot
